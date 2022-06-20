9987e8d965f4beaf019c4ccc22ae7a5ccbc84c23
ahmetozkanio <bledwarrior@gmail.com>
2022-06-20 10:39:27 +0300

    Initial Commit

---
+++ LandmarkBookSwiftUI/LandmarkBookSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarkBookSwiftUIApp.swift
+//  LandmarkBookSwiftUI
+//
+//  Created by Ahmet Ozkan on 20.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarkBookSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
